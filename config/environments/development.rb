1ad6879d11081250f6903045e4b9b2f5a5d13d93
Ashley Bye <ashley.bye85@gmail.com>
2014-07-29 01:21:30 +0100

    Fix user query mailer view problems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send. Actually, I do!
-  config.action_mailer.raise_delivery_errors = true
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -36,7 +33,9 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # ActionMailer settings
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',

ab858e3b997f40515e3b846e16ddfacddd4e978c
Ashley Bye <ashley.bye85@gmail.com>
2014-08-22 18:46:55 +0100

    Revert to using skrollr on mobile to allow scrollspy to function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,17 +44,23 @@
 	function adjustWindow(){
 
 		// Init Skrollr
-		if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
-			var s = skrollr.init({
-				forceHeight: false,
-				render: function(data) {
-					//Debugging - Log the current scroll position.
-					//console.log(data.curTop);
-				}
-			});
-		}
+		// if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+		// 	var s = skrollr.init({
+		// 		forceHeight: false,
+		// 		render: function(data) {
+		// 			//Debugging - Log the current scroll position.
+		// 			//console.log(data.curTop);
+		// 		}
+		// 	});
+		// }
 
-
+		var s = skrollr.init({
+			forceHeight: false,
+			render: function(data) {
+				//Debugging - Log the current scroll position.
+				//console.log(data.curTop);
+			}
+		});
 
 		// Get window size
 		winH = $window.height();
